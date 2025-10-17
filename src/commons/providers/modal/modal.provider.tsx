65ca1a033573b512addcd03811e4582f5587a35c
thetree601 <mytalk481@gmail.com>
2025-10-17 19:13:35 +0900

    일기쓰기 모달

--- src/commons/providers/modal/modal.provider.tsx
+++ src/commons/providers/modal/modal.provider.tsx
@@ -87,15 +87,7 @@ const ModalPortal: React.FC<ModalPortalProps> = ({
       className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
       onClick={handleBackdropClick}
     >
-      <div className="bg-white rounded-lg shadow-lg p-6 relative">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-        >
-          ✕
-        </button>
-        {children}
-      </div>
+      {children}
     </div>,
     modalRoot
   );
